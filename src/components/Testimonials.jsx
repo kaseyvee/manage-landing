993935cf2f49a49ad82459462d09fae3914a4a89
kethnal <kaseyvaldez@hotmail.com>
2023-04-13 02:13:36 -0700

    Add testimonials markup

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,9 +1,36 @@
+import data from "../data";
+
 function Testimonials() {
+
+  const testimonials = data.testimonials;
+
+  const testimonialsList = testimonials.map((testimonial) => {
+    return (
+      <div className="testimonials_list_item">
+        <div className="testimonials_list_item_image">
+          <img src={testimonial.image} alt={`headshot of ${testimonial.name}`} />
+        </div>
+
+        <h2 className="testimonials_list_item_name">
+          {testimonial.name}
+        </h2>
+
+        <p className="testimonials_list_item_testimony">
+          {testimonial.testimony}
+        </p>
+      </div>
+    )
+  })
+
   return (
     <div className="testimonials">
       <h1 className="testimonials_header">
         What they've said
       </h1>
+
+      <div className="testimonials_list">
+        {testimonialsList}
+      </div>
 
       <button className="button button-primary">Get Started</button>
     </div>
--- src/data.jsx
+++ src/data.jsx
@@ -16,19 +16,23 @@ const data = {
   testimonials: [
     {
       name: "Anisha Li",
-      testimony: "“Manage has supercharged our team’s workflow. The ability to maintain visibility on larger milestones at all times keeps everyone motivated.”"
+      testimony: "“Manage has supercharged our team’s workflow. The ability to maintain visibility on larger milestones at all times keeps everyone motivated.”",
+      image: "avatar-anisha.png"
     },
     {
       name: "Ali Bravo",
-      testimony: "“We have been able to cancel so many other subscriptions since using Manage. There is no more cross-channel confusion and everyone is much more focused.”"
+      testimony: "“We have been able to cancel so many other subscriptions since using Manage. There is no more cross-channel confusion and everyone is much more focused.”",
+      image: "avatar-ali.png"
     },
     {
       name: "Richard Watts",
-      testimony: "“Manage allows us to provide structure and process. It keeps us organized and focused. I can’t stop recommending them to everyone I talk to!”"
+      testimony: "“Manage allows us to provide structure and process. It keeps us organized and focused. I can’t stop recommending them to everyone I talk to!”",
+      image: "avatar-richard.png"
     },
     {
       name: "Shanai Gough",
-      testimony: "“Their software allows us to track, manage and collaborate on our projects from anywhere. It keeps the whole team in-sync without being intrusive.”"
+      testimony: "“Their software allows us to track, manage and collaborate on our projects from anywhere. It keeps the whole team in-sync without being intrusive.”",
+      image: "avatar-shanai.png"
     }
   ]
 }
