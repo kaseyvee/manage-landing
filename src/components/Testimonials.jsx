aaf0208088d639bda60bec18b1ac0ecfdf1a5774
kethnal <kaseyvaldez@hotmail.com>
2023-04-13 04:55:03 -0700

    Safety save

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,12 +1,18 @@
+import { useState } from "react";
 import data from "../data";
 
 function Testimonials() {
+  const [selectedTest, setSelectedTest] = useState(0)
+
+  const selectedStyle = {
+    backgroundColor: "hsl(12, 88%, 59%)"
+  }
 
   const testimonials = data.testimonials;
 
-  const testimonialsList = testimonials.map((testimonial) => {
+  const testimonialsList = testimonials.map((testimonial, i) => {
     return (
-      <div className="testimonials_list_item">
+      <div className="testimonials_list_item" id={i} key={i}>
         <div className="testimonials_list_item_image">
           <img src={testimonial.image} alt={`headshot of ${testimonial.name}`} />
         </div>
@@ -22,14 +28,29 @@ function Testimonials() {
     )
   })
 
+  const carouselItems = testimonials.map((testimonial, i) => {
+    return (
+      <a
+        style={selectedTest == i ? selectedStyle : {}}
+        href={`#${i}`}
+        key={i}
+        onClick={() => setSelectedTest(i)}
+      ></a>
+    )
+  })
+
   return (
-    <div className="testimonials">
+    <div className="testimonials" id="testimonials">
       <h1 className="testimonials_header">
         What they've said
       </h1>
 
       <div className="testimonials_list">
         {testimonialsList}
+      </div>
+
+      <div className="testimonials_carousel-nav">
+        {carouselItems}
       </div>
 
       <button className="button button-primary">Get Started</button>
